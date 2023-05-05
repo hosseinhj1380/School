1cea0025600c10e0db545e1c9c728e69bbf09f73
hosseinhj1380 <113828873+hosseinhj1380@users.noreply.github.com>
2023-05-05 19:01:23 +0330

    Update settings.py

--- School_Online/settings.py
+++ School_Online/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-4b_%$ml%d(piw$4!f(hv9rv4zr0zko@2d2gllg*c9&#!cb@q^%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
